f2bdaed8b6db60142eaa027961fec4060f3953c9
Alexander Aspman <aspmanalexander@gmail.com>
2022-10-28 08:35:36 +0200

    Initial Commit

---
+++ ADHDClockNotificationApp/ADHDClockNotificationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADHDClockNotificationAppApp.swift
+//  ADHDClockNotificationApp
+//
+//  Created by alexander aspman on 2022-10-28.
+//
+
+import SwiftUI
+
+@main
+struct ADHDClockNotificationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
